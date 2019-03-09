2c048b3357afb48845d8382e86cad8a2fda9dbe7
日野恵太 <keitahino0415@gmail.com>
2019-03-09 12:16:59 +0900

    add:execアクション追加

--- app/controllers/slackbot_controller.rb
+++ app/controllers/slackbot_controller.rb
@@ -1,22 +1,14 @@
 class SlackbotController < ApplicationController
   require 'slack-ruby-client'
   # callbackアクションのCSRFトークン認証を無効
-  protect_from_forgery :except => [:callback]
+  protect_from_forgery :except => [:exec]
+  before_action :slack_init, only: [:exec]
 
-  def callback
-    @body = JSON.parse(request.body.read)
-    return if !(@body['event']['channel'] == ENV['SLACK_SANDBOX_CHANEL'] ) || @body["event"].keys.include?("bot_id")
-    case @body['type']
-    when 'url_verification'
-      render json: @body["challenge"]
-    when 'event_callback'
-      slack_init
-      @client.chat_postMessage(
-        # as_user: 'true',
-        channel: @body['event']['channel'],
-        text: @body['event']['text']
-      )
-    end
+  def exec
+    @client.chat_postMessage(
+      channel: params['channel_id'],
+      text: "test"
+    )
   end
 
   private
