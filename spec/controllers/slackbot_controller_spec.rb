2ba7b4494a8bb65997c051fcb269d2daee21c043
日野恵太 <keitahino0415@gmail.com>
2019-03-09 14:51:17 +0900

    rm:テストを一旦コメントアウト

--- spec/controllers/slackbot_controller_spec.rb
+++ spec/controllers/slackbot_controller_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe SlackbotController, type: :controller do
 
-  describe "GET #callback" do
-    it "returns http success" do
-      # get :callback
-      # expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #callback" do
+  #   it "returns http success" do
+  #     # get :callback
+  #     # expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
